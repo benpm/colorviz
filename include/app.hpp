0cdf00c75bbbb4b3217c91e13f4d70298c8c7c20
benpm <schwalegos@gmail.com>
2024-04-10 11:23:58 -0600

    cam working

--- include/app.hpp
+++ include/app.hpp
@@ -15,11 +15,25 @@ class App
     GLuint vao, vbo, ebo;
     ShaderProgram program;
     Camera cam;
+    CameraControl camCtrl;
     std::shared_ptr<GamutData> data;
+    struct Mouse
+    {
+        Vector2f pos = Vector2f::Zero();
+        Vector2f lastPos = Vector2f::Zero();
+        Vector2f delta = Vector2f::Zero();
+        Vector2f dragStart = Vector2f::Zero();
+        Vector2f dragDelta = Vector2f::Zero();
+        float scroll = 0.0f;
+        bool left = false;
+        bool right = false;
+    } mouse;
 
     App(Vector2f winSize);
+    // Called before event processing
     void prepare();
     void update(float time, float delta);
     void draw(float time, float delta);
     void event(const GLEQevent& event);
+    void onMouseButton(int button, bool pressed);
 };
--- src/app.cpp
+++ src/app.cpp
@@ -2,6 +2,7 @@
 
 App::App(Vector2f winSize)
 {
+    glfwSwapInterval(1);
     glEnable(GL_DEBUG_OUTPUT) $glChk;
     glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS) $glChk;
     glEnable(GL_DEPTH_TEST) $glChk;
@@ -28,14 +29,31 @@ App::App(Vector2f winSize)
     program.setVertexAttrib(vbo, "vPos", 3, GL_FLOAT, 0u, 0u);
     gfx::setbuf(GL_ELEMENT_ARRAY_BUFFER, ebo, data->triangles);
 
+    camCtrl.mode = CameraControl::Mode::orbit;
+    camCtrl.orbitDist(150.0f);
     cam.pos = { 0.0f, 0.0f, 2.0f };
     cam.viewSize = winSize;
 }
 
-void App::prepare() {}
+void App::prepare()
+{
+    mouse.scroll = 0.0f;
+}
 
 void App::update(float time, float delta)
 {
+    mouse.delta = mouse.pos - mouse.lastPos;
+    mouse.dragDelta = Vector2f(mouse.dragStart - mouse.pos) * (float)mouse.left * 2.0f;
+    mouse.dragDelta.y() *= -1.0f;
+    mouse.lastPos = mouse.pos;
+
+    Vector2f screenDragDelta = mouse.delta * (float)mouse.left;
+    camCtrl.control(
+        screenDragDelta.cwiseQuotient(cam.viewSize), mouse.dragDelta.cwiseQuotient(cam.viewSize),
+        { 0.0f, 0.0f }
+    );
+    camCtrl.universalZoom(mouse.scroll);
+    camCtrl.update(cam, cam.viewSize);
     Matrix4f model = Matrix4f::Identity();
     program.setUniform("uTModel", model);
     program.setUniform("uTView", cam.getView());
@@ -62,6 +80,33 @@ void App::event(const GLEQevent& event)
         case GLEQ_WINDOW_RESIZED:
             cam.viewSize.x() = event.size.width;
             cam.viewSize.y() = event.size.height;
+            glViewport(0, 0, event.size.width, event.size.height) $glChk;
             break;
+        case GLEQ_CURSOR_MOVED:
+            mouse.pos = { event.pos.x, event.pos.y };
+            break;
+        case GLEQ_BUTTON_PRESSED:
+            onMouseButton(event.mouse.button, true);
+            break;
+        case GLEQ_BUTTON_RELEASED:
+            onMouseButton(event.mouse.button, false);
+            break;
+        case GLEQ_SCROLLED:
+            mouse.scroll += -event.scroll.y * 0.1f;
+            break;
+    }
+}
+
+void App::onMouseButton(int button, bool pressed)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+        if (mouse.left ^ pressed) {
+            // Mouse button just pressed
+            camCtrl.dragStart();
+            mouse.dragStart = mouse.pos;
+        }
+        mouse.left = pressed;
+    } else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+        mouse.right = pressed;
     }
 }
